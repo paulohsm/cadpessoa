0bc34aeb16969359619885117ba2bacd0db45481
Paulo Santiago <paulohsm@gmail.com>
2024-02-17 21:43:41 -0300

    Adicionada a classe "Pessoa" para representar os dados.

---
+++ src/main/java/com/github/paulohsm/cadpessoa/entities/Pessoa.java
@@ -0,0 +1,39 @@
+package com.github.paulohsm.cadpessoa.entities;
+
+public class Pessoa {
+    private Long id;
+    private String nome;
+    private String email;
+
+    public Pessoa() {}
+
+    public Pessoa(Long id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

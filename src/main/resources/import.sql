f4bc9e570570af6fc95ede25c542c72d2920404e
Paulo Santiago <paulohsm@gmail.com>
2024-02-17 22:18:06 -0300

    Alimentando o banco de dados H2.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT into TB_PESSOA (nome, email) VALUES ('Paulo', 'paulo@henrique.com');
+INSERT into TB_PESSOA (nome, email) VALUES ('Alisson', 'jose@youssf.com');
+INSERT into TB_PESSOA (nome, email) VALUES ('Gabriel', 'gana@biel.com');
